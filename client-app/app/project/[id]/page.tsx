7c38f53a1e8ea221f8f819d9182715cfbe761f17
Freddy Guitton <freddy@wave.ai>
2024-07-15 10:03:45 +0200

    Add delete button

--- client-app/app/project/[id]/page.tsx
+++ client-app/app/project/[id]/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { Project, getProject } from "@/app/api/projects"
+import { Project, deleteProject, getProject } from "@/app/api/projects"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 
 
 export default function ProjectDetails({ params }: { params: { id: number } }) {
+  const router = useRouter()
   const [project, setProject] = useState<Project | null>(null)
+  const [deleting, setDeleting] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,6 +18,13 @@ export default function ProjectDetails({ params }: { params: { id: number } }) {
     }
     fetchData()
   }, [params.id])
+
+  const deleteProjectHandler = async () => {
+    setDeleting(true)
+    await deleteProject(params.id)
+    setDeleting(false)
+    router.replace("/")
+  }
   return (
     <section>
       {
@@ -22,6 +32,8 @@ export default function ProjectDetails({ params }: { params: { id: number } }) {
         (
           <div>
             <h1>{project.name}</h1>
+            <button onClick={deleteProjectHandler} disabled={deleting}>Delete</button>
+            <hr/>
             <h2>Partners: {project.partners.join(", ")}</h2>
             <h2>Patients count: {project.patientsCount}</h2>
             <h2>Age average: {project.patientsAgeAverage}</h2>
